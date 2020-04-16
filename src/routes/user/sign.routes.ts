96d2425d62e1c595f0dc0cd86dd10e29eb721fd9
SirReiva <gjjavicoru@hotmail.com>
2020-04-16 21:58:13 +0200

    add route resign

--- src/routes/user/sign.routes.ts
+++ src/routes/user/sign.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { signIn, signUp } from '../../controllers/user.controller';
+import { signIn, signUp, reSignIn } from '../../controllers/user.controller';
 import { handlerExceptionRoute } from '../../error/index';
-import { MemCacheMiddleware } from '../../middlewares/cache/mem-cache.middelware';
 
 const ROUTE_PATH = '/sign'
 
 const router = Router();
 
 router
-    .post(ROUTE_PATH +'up', MemCacheMiddleware(240, 'users'), handlerExceptionRoute(signUp))
-    .post(ROUTE_PATH + 'in', handlerExceptionRoute(signIn));
+    .post(ROUTE_PATH +'up', handlerExceptionRoute(signUp))
+    .post(ROUTE_PATH + 'in', handlerExceptionRoute(signIn))
+    .post(ROUTE_PATH + 'refresh', handlerExceptionRoute(reSignIn))
 
 export default router;
