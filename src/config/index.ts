54fac8566a6b016c8ebcb358eb0836ca588d6375
SirReiva <gjjavicoru@hotmail.com>
2020-04-17 11:20:31 +0200

    config added

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+export default {
+    JWTSECRET: "secret",
+    JWTEXPIRATION: 3600,
+    JWTEXPIRATIONREFRESH: 126000,
+    DB: {
+        URL: "mongodb://localhost/cms",
+        USER: "root",
+        PASSWORD: "root"
+    },
+    VERSION: "v1"
+};
