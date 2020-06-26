463a46ca310bed65360e3dccd44a9f084e6105f0
SirReiva <gjjavicoru@hotmail.com>
2020-06-26 12:35:56 +0200

    fix validator

--- src/validators/post.validator.ts
+++ src/validators/post.validator.ts
@@ -4,7 +4,7 @@ import { ValidationSchema } from 'fastest-validator';
 const CreatePostSchema: ValidationSchema = {
     title: { type: 'string', min: 3, max: 255, required: true },
     content: { type: 'string', required: true },
-    isPublic: { type: 'boolean' },
+    isPublic: { type: 'boolean', required: false },
     $$strict: true,
 };
 
